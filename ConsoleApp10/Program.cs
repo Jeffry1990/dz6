e00a3ee0f035ff7be3e59b242dfc88d3b97aadf1
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-12 19:50:31 +0300

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int crystalCount;
+            int crystalPrice = 15;
+
+            Console.Write("Введите кол-во золота: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов вы хотите купить: ");
+            crystalCount = Convert.ToInt32(Console.ReadLine());
+            money -= crystalCount * crystalPrice;
+
+            Console.WriteLine("У вас - " + money + " золота и " + crystalCount + " кристаллов.");
+        }
+    }
+}
